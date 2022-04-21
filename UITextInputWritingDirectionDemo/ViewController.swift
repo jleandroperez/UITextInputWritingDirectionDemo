bcd48095db4cd724a274b2dc1557b5b90633cd52
Jorge Leandro Perez <jorge@lantean.co>
2022-04-21 11:26:54 -0300

    Initial Commit

---
+++ UITextInputWritingDirectionDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITextInputWritingDirectionDemo
+//
+//  Created by Jorge Leandro Perez on 4/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
